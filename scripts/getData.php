b0bb40f8fbf5d954f492f525f7bc3e06e73d7eb7
David Meier <david.meier127@gmail.com>
2013-09-27 09:23:54 -0500

    I added my get assignment function.

--- scripts/getData.php
+++ scripts/getData.php
@@ -1,10 +1,46 @@
 <?php
 require 'connect.php';
+
+function getStudent() {
+}
+
+function getStudents() {
+}
+
+function getRequest() {
+}
+
+function getRequests() {
+}
+
+function getAssignment($requestID, $volunteerID)
+{
+	$result = mysqli_query($con,"SELECT * FROM assignments");
+	while($row = mysqli_fetch_array($result)) {
+		if ($requestID == $row['requestID'] && $volunteerID == $row['volunteerID']) {
+ 		$assignID = $row['assignmentID'];
+  		}
+	}
+	return $assignID;
+}
+
 function getAssignments($con){
 	$sql="SELECT * FROM assignments"
 	$result = mysqli_query($con,$sql);
 	echo $result;
 	return $result;
 }
+
+function getRequestAssignments($ID) {
+
+}
+
+function getRecommended(){
+
+}
+
+//getAssignment('$_POST[volunteerID]','$_POST[requestID]');
+
+?>
 
 ?>
